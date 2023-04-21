f78dd041fa74e521966e360ae16377b6ca1255aa
Andrey <vosirandr@gmail.com>
2023-04-21 22:21:10 +0300

    add static files

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 
 const port = 8080
 
 app.set("view engine", "pug");
-app.set("views"); // установка пути к представлениям. По умолчанию - views
+app.set("views");
 
-app.use("/", function(request, response){
-    response.render("index");
+app.use("/", function(req, res){
+    res.render("index");
+    res.setHeader('content-type', 'text/plain');
 }); 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use("/static", express.static(path.join(__dirname, 'static')));
 
-app.listen(port, () => console.log('http://localhost:${8080}'));
+app.listen(port, () => console.log(`http://localhost:${8080}`));
