f165c6a78f4f5a38f0d89909df4683ff19afdc58
Andrey <vosirandr@gmail.com>
2023-04-21 12:06:19 +0300

    update index and add favicon

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-  
 const app = express();
-  
+
+const port = 8080
+
 app.set("view engine", "pug");
 app.set("views"); // установка пути к представлениям. По умолчанию - views
- 
+
 app.use("/", function(request, response){
-      
     response.render("index");
 }); 
 app.use(express.static(path.join(__dirname, 'views')));
 
-app.listen(3000, () => console.log('http://localhost:3000'));
+app.listen(port, () => console.log('http://localhost:${8080}'));
